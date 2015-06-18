3ae3cc74996ff8f76188b651176769d48e533b24
Thomas Stensitzki <thomas@mcsmemail.de>
2015-06-18 10:42:49 +0200

    Copy permissions added, DomainController parameter added

--- Copy-ReceiveConnector.ps1
+++ Copy-ReceiveConnector.ps1
@@ -1,13 +1,13 @@
 <# 
     .SYNOPSIS 
-    Copy a selected receive connectors and it's configuration to other Exchange Servers
+    Copy a selected receive connector and it's configuration and permissions to other Exchange Servers
 
     Thomas Stensitzki 
 
     THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
     RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
 
-    Version 1.0, 2014-12-03
+    Version 1.1, 2015-06-18
 
     Please send ideas, comments and suggestions to support@granikos.eu 
 
@@ -16,7 +16,9 @@
 
     .DESCRIPTION 
     This script copies a receive connector from a source Exchange Server to a single target
-    Exchange server or to all Exchange servers 
+    Exchange server or to all Exchange servers.
+    
+    Configured permissions are copied as well 
  
     .NOTES 
     Requirements 
@@ -24,30 +26,47 @@
     
     Revision History 
     -------------------------------------------------------------------------------- 
-    1.0 Initial community release 
+    1.0      Initial community release 
+    1.1      Domain Controller parameter added, permissions group copy added
 
     .PARAMETER ConnectorName  
     Name of the connector the new IP addresses should be added to  
 
-    .EXAMPLE 
-    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC2 -TargetServer MBX2
+    .PARAMETER SourceServer
+    Name of the receive connector to copy
+
+    .PARAMETER TargetServer
+    Target Exchange server to copy the selected receive connector to
+
+    .PARAMETER DomainController
+    Domain Controller name
+
+    .PARAMETER CopyToAllOther
+    Switch to copy to all other Exchange servers
 
     .EXAMPLE 
-    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC1 -CopyToAllOther
+    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC2 -TargetServer MBX2 -DomainController MYDC1.mcsmemail.de
+
+    .EXAMPLE 
+    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC1 -CopyToAllOther -DomainController MYDC1.mcsmemail.de
 #> 
 
 param(
-	[parameter(Mandatory=$true,HelpMessage='Source Exchange server to copy from',ParameterSetName="CRC")]
+	[parameter(Mandatory=$true,HelpMessage='Source Exchange server to copy from')]
 		[string] $SourceServer,
-	[parameter(Mandatory=$true,HelpMessage='Name of the receive connector to copy',ParameterSetName="CRC")]
+	[parameter(Mandatory=$true,HelpMessage='Name of the receive connector to copy')]
 		[string] $ConnectorName,
-	[parameter(Mandatory=$false,HelpMessage='Target Exchange server to copy the selected receive connector to',ParameterSetName="CRC")]
+	[parameter(Mandatory=$false,HelpMessage='Target Exchange server to copy the selected receive connector to')]
 		[string] $TargetServer = "",
-    [parameter(Mandatory=$false,HelpMessage='Copy to all other Exchange servers',ParameterSetName="CRC")]
+	[parameter(Mandatory=$true,HelpMessage='Domain Controller name')]
+		[string] $DomainController = "",
+    [parameter(Mandatory=$false,HelpMessage='Copy to all other Exchange servers')]
         [switch] $CopyToAllOther
 )
 
 Set-StrictMode -Version Latest
+
+Import-Module ActiveDirectory 
 
 $sourceRC = $null
 
@@ -62,6 +81,12 @@ function CopyToServer([string]$TargetServerName) {
     if(($sourceRC -ne $null) -and ($targetRC -eq $null)){
         Write-Verbose "Adding new receive connector to $serverName"
 
+        # clear permission groups for Exchange Server 2013 (thanks to Jeffery Land, https://jefferyland.wordpress.com)
+        $tempPermissionGroups = @($sourceRC.PermissionGroups) -split ", " | Select-String -Pattern "Custom" -NotMatch
+        $temp = "$($tempPermissionGroups)"
+        $sourceRC.PermissionGroups = $temp.Replace(" ", ", ")
+
+        # create new Receive Connector
         New-ReceiveConnector -Name $sourceRC.Name `
         -TransportRole $sourceRC.TransportRole `
         -RemoteIPRanges $sourceRC.RemoteIPRanges `
@@ -97,9 +122,15 @@ function CopyToServer([string]$TargetServerName) {
         -EnableAuthGSSAPI $sourceRC.EnableAuthGSSAPI `
         -ExtendedProtectionPolicy $sourceRC.ExtendedProtectionPolicy `
         -SizeEnabled $sourceRC.SizeEnabled `
-        -TarpitInterval $sourceRC.TarpitInterval `
-        -EnhancedStatusCodesEnabled  $sourceRC.EnhancedStatusCodesEnabled `
-        -Server $TargetServerName 
+        -TarpitInterval $sourceRC.TarpitInterval `        -EnhancedStatusCodesEnabled  $sourceRC.EnhancedStatusCodesEnabled `        -Server $TargetServerName 
+
+        # fetch non inherited permissons from source connector
+        $sourcePermissions = Get-ReceiveConnector -Identity $sourceRC | Get-ADPermission | where {$_.IsInherited -eq $false}
+
+        # set access rights on target connector
+        $sourcePermissions | foreach {
+             Get-ReceiveConnector "$($TargetServerName)\$($sourceRC.Name)" | Add-ADPermission -DomainController $DomainController -User $_.User -Deny:$_.Deny -AccessRights $_.AccessRights -ExtendedRights $_.ExtendedRights
+        }
     }
     else {
         Write-Output "Receive connector is null or target connector already exists, nothing to do here."
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Copy-ReceiveConnector
-Copy Exchange Server 2013 receive connector settings
+    .SYNOPSIS 
+    Copy a selected receive connector and it's configuration and permissions to other Exchange Servers
+
+    Thomas Stensitzki 
+
+    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE  
+    RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER. 
+
+    Version 1.1, 2015-06-18
+
+    Please send ideas, comments and suggestions to support@granikos.eu 
+
+    .LINK 
+    More information can be found at http://www.granikos.eu/en/scripts
+
+    .DESCRIPTION 
+    This script copies a receive connector from a source Exchange Server to a single target
+    Exchange server or to all Exchange servers.
+    
+    Configured permissions are copied as well 
+ 
+    .NOTES 
+    Requirements 
+    - Windows Server 2008 R2 SP1, Windows Server 2012 or Windows Server 2012 R2  
+    
+    Revision History 
+    -------------------------------------------------------------------------------- 
+    1.0      Initial community release 
+    1.1      Domain Controller parameter added, permissions group copy added
+
+    .PARAMETER ConnectorName  
+    Name of the connector the new IP addresses should be added to  
+
+    .PARAMETER SourceServer
+    Name of the receive connector to copy
+
+    .PARAMETER TargetServer
+    Target Exchange server to copy the selected receive connector to
+
+    .PARAMETER DomainController
+    Domain Controller name
+
+    .PARAMETER CopyToAllOther
+    Switch to copy to all other Exchange servers
+
+    .EXAMPLE 
+    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC2 -TargetServer MBX2 -DomainController MYDC1.mcsmemail.de
+
+    .EXAMPLE 
+    .\Copy-ReceiveConnector.ps1 -SourceServer MBX01 -ConnectorName nikos-one-RC1 -CopyToAllOther -DomainController MYDC1.mcsmemail.de
